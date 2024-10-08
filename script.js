94992949bf5d5e56613e61151699379f0675a13a
ayush1608-git <ayushkaradbhajne2002@gmail.com>
2024-10-08 18:59:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+    var typed = new Typed('#element', {
+        strings: ['Web Developer'],
+        typeSpeed: 50,
+    });
